e94001995df21c7c6cdcd4d7d2d6ea3c3a832796
tetsu <soutetsu@gmail.com>
2014-12-27 11:17:35 +0900

    > categories テーブルの構造を変更.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141227015553) do
+ActiveRecord::Schema.define(version: 20141227021543) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
