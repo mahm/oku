b568f3857b283afe1f84837397ad2d2d847b041c
tetsu <soutetsu@gmail.com>
2014-12-27 11:38:18 +0900

    items テーブルの構造を変更.

--- db/migrate/20141227023511_create_items.rb
+++ db/migrate/20141227023511_create_items.rb
@@ -1,9 +1,9 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
-      t.references :user, index: true
-      t.references :category, index: true
-      t.string :name
+      t.references :user, index: true, null: false
+      t.references :category, index: true, null: false
+      t.string :name, null: false
       t.text :explanation
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141227021543) do
+ActiveRecord::Schema.define(version: 20141227023511) do
 
   create_table "categories", force: true do |t|
     t.string   "name",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "items", force: true do |t|
+    t.integer  "user_id",     null: false
+    t.integer  "category_id", null: false
+    t.string   "name",        null: false
+    t.text     "explanation"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "items", ["category_id"], name: "index_items_on_category_id"
+  add_index "items", ["user_id"], name: "index_items_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
