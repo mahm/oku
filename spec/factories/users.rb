e1f9bf22ad3eb8d9e790656f72856c38c322ec1f
tetsu <soutetsu@gmail.com>
2014-12-27 12:01:55 +0900

    users モデルのテストコードを作成.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:email) { |n| "dummy#{n}@example.com" }
+    password 'x' * 8
   end
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a valid email address and password made by 8 or more characters' do
+    expect(build(:user)).to be_valid
+  end
+
+  it 'is invalid without a email address' do
+    user = build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it 'is invalid with a invalid email address' do
+    user = build(:user, email: 'This is not a email address')
+    user.valid?
+    expect(user.errors[:email]).to include("is invalid")
+  end
+
+  it 'is invalid without a password' do
+    user = build(:user, password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
+  end
+
+  it 'is invalid with a password less than 7 characters' do
+    user = build(:user, password: 'x' * 7)
+    user.valid?
+    expect(user.errors[:password]).to include("is too short (minimum is 8 characters)")
+  end
 end
