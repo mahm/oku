c835b56df5ae3ef0d44f2af09bcb19c4b625d3c5
tetsu <soutetsu@gmail.com>
2014-12-28 16:16:54 +0900

    user と bid との関連を定義.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :items
   has_many :auctions
+  has_many :bids
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
