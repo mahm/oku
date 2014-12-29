a8afa10a31ab2538dcf6038150f673ec457e0018
tetsu <soutetsu@gmail.com>
2014-12-29 12:21:57 +0900

    db/seeds.rb を修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Category.delete_all
 Category.create([
-                    { name: 'その他' },
-                    { name: 'PC・周辺機器' },
-                    { name: '家電・AV・カメラ' },
-                    { name: '本・雑誌' },
-                    { name: 'ビデオ' },
-                    { name: 'おもちゃ・ゲーム' },
-                    { name: 'ファッション' },
-                    { name: 'アクセサリー・時計' },
-                    { name: '雑貨・文房具' },
-                    { name: '食品・飲料' }
+                    { code: 1000, name: 'PC・周辺機器' },
+                    { code: 1100, name: '家電・AV・カメラ' },
+                    { code: 1200, name: '本・雑誌' },
+                    { code: 1300, name: 'ビデオ' },
+                    { code: 1400, name: 'おもちゃ・ゲーム' },
+                    { code: 1500, name: 'ファッション' },
+                    { code: 1600, name: 'アクセサリー・時計' },
+                    { code: 1700, name: '雑貨・文房具' },
+                    { code: 1800, name: '食品・飲料' },
+                    { code: 9999, name: 'その他' }
                 ])
