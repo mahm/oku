a48fa7deca5c3df29f110ae779fde100cc6da1f4
tetsu <soutetsu@gmail.com>
2014-12-29 11:40:08 +0900

    db への初期データ追加スクリプトを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.delete_all
+Category.create([
+                    { name: 'その他' },
+                    { name: 'PC・周辺機器' },
+                    { name: '家電・AV・カメラ' },
+                    { name: '本・雑誌' },
+                    { name: 'ビデオ' },
+                    { name: 'おもちゃ・ゲーム' },
+                    { name: 'ファッション' },
+                    { name: 'アクセサリー・時計' },
+                    { name: '雑貨・文房具' },
+                    { name: '食品・飲料' }
+                ])
