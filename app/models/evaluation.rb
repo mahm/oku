a203a0ff94987503e11e2f6eab717f05260456f7
tetsu <soutetsu@gmail.com>
2015-01-06 08:53:43 +0900

    evaluation モデルのバリデーション及び user モデルとの関連を定義

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,2 +1,7 @@
 class Evaluation < ActiveRecord::Base
+  belongs_to :evaluater, class_name: 'User'
+  belongs_to :evaluatee, class_name: 'User'
+
+  validates :evaluater_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :evaluatee_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :items
   has_many :auctions
   has_many :bids
+  has_many :evaluations, foreign_key: 'evaluater_id', dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
