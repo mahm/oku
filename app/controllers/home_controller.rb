e379a971a2d8340e780c9129c364c1a3cbebaddb
tetsu <soutetsu@gmail.com>
2015-02-08 17:01:48 +0900

    ログイン後にトップページにアクセスできない不具合を修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
   def index
-    if user_signed_in?
-      redirect_to my_auctions_path
-    else
-      redirect_to auctions_path
-    end
+    redirect_to auctions_path
   end
 end
