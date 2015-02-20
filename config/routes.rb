488c27b6e72235431d45a69b975a76a954ee2c49
tetsu <soutetsu@gmail.com>
2015-02-20 15:22:42 +0900

    routes 変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
+  get 'bids/index'
+
+  get 'bids/new'
+
+  get 'bids/create'
+
+  get 'bids/destroy'
+
   root to: 'home#index'
 
   devise_for :users
 
-  resources :auctions, only: %i(index show)
+  resources :auctions, only: %i(index show) do
+    resources :bids, only: %i(index new create destroy)
+  end
 
   namespace :my do
     resources :auctions
