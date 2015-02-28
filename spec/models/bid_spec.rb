5d2757f9b360bacd22fb4eac564a10c70e145125
tetsu <soutetsu@gmail.com>
2015-02-28 13:39:26 +0900

    bid_spec を変更

--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Bid, :type => :model do
-  it 'is invalid without a auction_id' do
+  it 'auction_id 入力必須' do
     bid = build(:bid, auction_id: nil)
     bid.valid?
     expect(bid.errors[:auction_id]).to include("can't be blank")
   end
 
-  it 'is invalid without a user_id' do
+  it 'user_id 入力必須' do
     bid = build(:bid, user_id: nil)
     bid.valid?
     expect(bid.errors[:user_id]).to include("can't be blank")
   end
 
-  it 'is invalid without a price' do
+  it 'price 入力必須' do
     bid = build(:bid, price: nil)
     bid.valid?
     expect(bid.errors[:price]).to include("can't be blank")
   end
 
-  it 'is invalid when price is less than 0' do
+  it 'price が 0 以上でなければならない' do
     bid = build(:bid, price: -1)
     bid.valid?
     expect(bid.errors[:price]).to include("must be greater than or equal to 0")
   end
 
-  it 'is valid with a auction_id, user_id, price and auction is in time' do
+  it 'オークションが開催期間内の入札は有効' do
     auction = create(:auction, open_at: Time.now + 1.day, close_at: Time.now + 3.day)
     bidder = create(:user)
     travel 2.day
@@ -34,7 +34,7 @@ RSpec.describe Bid, :type => :model do
     expect(bid).to be_valid
   end
 
-  it 'is invalid when auction is over' do
+  it 'オークション終了日時後の入札はできない' do
     auction = create(:auction, open_at: Time.now + 1.day, close_at: Time.now + 3.day)
     bidder = create(:user)
     travel 3.day
@@ -43,7 +43,15 @@ RSpec.describe Bid, :type => :model do
     expect(bid.errors[:auction_id]).to include("auction is over")
   end
 
-  it 'is invalid when auction is not open' do
+  it 'オークション終了日時後でも、落札処理（入札データの保存）は可能' do
+    auction = create(:auction, open_at: Time.now + 1.day, close_at: Time.now + 3.day)
+    bidder = create(:user)
+    travel 3.day
+    bid = Bid.new(auction_id: auction.id, user_id: bidder.id, price: 1, accepted: true)
+    expect(bid).to be_valid
+  end
+
+  it '開催前のオークションへは入札できない' do
     auction = create(:auction, open_at: Time.now + 1.day, close_at: Time.now + 3.day)
     bidder = create(:user)
     bid = Bid.new(auction_id: auction.id, user_id: bidder.id, price: 1)
@@ -51,7 +59,7 @@ RSpec.describe Bid, :type => :model do
     expect(bid.errors[:auction_id]).to include("auction is not open")
   end
 
-  it 'is invalid when price is not highest' do
+  it '現在の最高入札額以下の入札はできない' do
     auction = create(:auction, open_at: Time.now + 1.day, close_at: Time.now + 7.day)
     travel 2.day
     create(:bid, auction_id: auction.id, price: 100)
