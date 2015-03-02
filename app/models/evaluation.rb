9098ff61b5288c10ccb3675adb887e54aaaf13b7
tetsu <soutetsu@gmail.com>
2015-03-02 18:05:39 +0900

    出品者の評価でポイント（良い・悪い）も保存できるようにした

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,4 +1,7 @@
 class Evaluation < ActiveRecord::Base
+  extend Enumerize
+  enumerize :point, in: { bad: -1, good: 1 }
+
   belongs_to :auction
   belongs_to :evaluater, class_name: 'User'
   belongs_to :evaluatee, class_name: 'User'
