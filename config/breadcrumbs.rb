08dd1d4d91ed5efb4e4e269c4133d577013ea887
tetsu <soutetsu@gmail.com>
2015-03-06 15:45:36 +0900

    パンくずリストを実装

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,28 +1,23 @@
 crumb :root do
-  link "Home", root_path
+  link 'オークション一覧', root_path
 end
 
-# crumb :projects do
-#   link "Projects", projects_path
-# end
+crumb :category do |category|
+  link category.name, [category, :auctions]
+  parent :root
+end
 
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
+crumb :auction do |auction|
+  link auction.title, [auction.category, auction]
+  parent :category, auction.category
+end
 
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
+crumb :bids do |auction|
+  link '入札 一覧'
+  parent :auction, auction
+end
 
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
-
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
+crumb :bid do |bid|
+  link '入札'
+  parent :auction, bid.auction
+end
