70b0ec139b06e99becae92819f1588d23e137178
tetsu <soutetsu@gmail.com>
2015-03-07 17:38:21 +0900

    Faker が i18n locale=:ja で不正な email を出力する問題を解決

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:email) { Faker::Internet.email('dummy') }
+    sequence(:email) {
+      Faker::Config.locale = :en
+      Faker::Internet.email('dummy')
+     }
     password 'x' * 8
   end
 end
