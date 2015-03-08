42a3cdb2b3acaca9ccd224e0c78960b36a4e9495
tetsu <soutetsu@gmail.com>
2015-03-08 22:33:15 +0900

    cosme

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :rememberable, :validatable
 
   has_many :bids
-  has_many :owned_auctions, class_name: 'Auction' # 自分が開催しているオークション
-  has_many :bidded_auctions, through: :bids, source: :auction # 他人が開催し、自分が入札しているオークション
+  # 自分が開催しているオークション
+  has_many :owned_auctions, class_name: 'Auction'
+  # 他人が開催し、自分が入札しているオークション
+  has_many :bidded_auctions, through: :bids, source: :auction
+  # 自分が他人に対して行った評価
   has_many :evaluations, foreign_key: 'evaluater_id', dependent: :destroy
-  has_many :evaluatees, through: :evaluations
+  # 他人が自分に対して行った評価
   has_many :reverse_evaluations, foreign_key: 'evaluatee_id', class_name: 'Evaluation', dependent: :destroy
+  # 出品（被評価）者
+  has_many :evaluatees, through: :evaluations
+  # 評価（落札）者
   has_many :evaluaters, through: :reverse_evaluations
 
   def accepted_auctions
