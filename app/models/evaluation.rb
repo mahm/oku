672495b8eaaad9ee4a27d2b3cef46087a554dfbc
tetsu <soutetsu@gmail.com>
2015-03-18 22:33:56 +0900

    cosme

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,12 +1,12 @@
 class Evaluation < ActiveRecord::Base
   extend Enumerize
-  enumerize :point, in: { bad: -1, good: 1 }, scope: true
+  enumerize :point, in: {bad: -1, good: 1}, scope: true
 
   belongs_to :auction
   belongs_to :evaluater, class_name: 'User'
   belongs_to :evaluatee, class_name: 'User'
 
   validates :auction_id, presence: true, numericality: {only_integer: true, greater_than: 0}
-  validates :evaluater_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :evaluatee_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :evaluater_id, presence: true, numericality: {only_integer: true, greater_than: 0}
+  validates :evaluatee_id, presence: true, numericality: {only_integer: true, greater_than: 0}
 end
