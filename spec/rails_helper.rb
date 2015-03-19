07909a9165372493e8692eff0a45f82f39859afb
tetsu <soutetsu@gmail.com>
2015-03-19 23:57:55 +0900

    rspec 設定変更

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,7 +18,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -51,4 +51,5 @@ RSpec.configure do |config|
   # Includes
   config.include FactoryGirl::Syntax::Methods
   config.include ActiveSupport::Testing::TimeHelpers
+  config.include FeatureHelper, type: :feature
 end
