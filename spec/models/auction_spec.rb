902437715b1a9a081eeaaf011c16db2f8f8ce6ed
tetsu <soutetsu@gmail.com>
2015-03-21 11:53:01 +0900

    update auction model spec

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -46,4 +46,26 @@ RSpec.describe Auction, :type => :model do
     auction.valid?
     expect(auction.errors[:open_at]).not_to be_empty
   end
+
+  describe 'オークション終了処理' do
+    context '入札が 1 件以上存在する場合' do
+      before(:each) do
+        @auction = create(:auction, open_at: Time.now+1.hour, close_at: Time.now+2.hour)
+        bidder = create(:user)
+        travel 1.hour
+        create(:bid, auction_id: @auction.id, user_id: bidder.id, price: @auction.first_price+1)
+        create(:bid, auction_id: @auction.id, user_id: bidder.id, price: @auction.bids.order(:price).last.price+1)
+        travel 2.hour
+        @auction.accept_and_close
+      end
+      it 'closed が true となる' do
+        expect(@auction.closed).to be_truthy
+      end
+      it '最高入札額が落札価格となる' do
+        highest_bid = @auction.bids.order(:price).last
+        expect(highest_bid.accepted).to be_truthy
+        expect(@auction.accepted_price).to eq(highest_bid.price)
+      end
+    end
+  end
 end
